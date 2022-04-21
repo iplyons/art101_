307e7ad1bc638a8c6fa9cc69a2495bf1c172aa5e
Izzy Lyons <iplyons@ucsc.edu>
2022-04-20 19:10:38 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+/**
+* Summary. (use period)
+*
+* Description. (use period)
+*
+* @link   URL
+* @file   This files defines the MyClass class.
+* @author AuthorName.
+* @since  x.x.x
+*/
+
+/** jshint {inline configuration here.} */
+// Print picture
+    // Divide into 16 boxes
+    // Set box position
+  //Randomize
+    // Change box position from original
+  // Accept user input
+  // Validate user move
+  // End game when boxes are back to original position 
