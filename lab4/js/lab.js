ba93d14cf9250b82a34174c2d48d68787d4d40cf
Izzy Lyons <iplyons@ucsc.edu>
2022-04-22 21:02:08 -0700

    u

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,30 @@
 /**
-* Summary. (use period)
+* Change a tire
+Find spare
+Jack car
+Take original tire off
+Put new tire on
+Make 4D art
+Wake up early
+Make a pie
+Get ingredients
+Find recipe
+Follow recipe
+Plant tomatoes
+Dig hole
+Plant seed
+Water
+. (use period)
 *
-* Description. (use period)
+* Make a 4x4 puzzle
+Print picture
+Divide into 16 boxes
+Set box position
+Randomize from original
+Accept user input
+Validate user move
+End game when boxes are back to original position
+. (use period)
 *
 * @link   URL
 * @file   This files defines the MyClass class.
@@ -17,4 +40,4 @@
     // Change box position from original
   // Accept user input
   // Validate user move
-  // End game when boxes are back to original position 
+  // End game when boxes are back to original position
---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+* Author: Izzy Lyons <iplyons@ucsc.edu>
+* Created: 22 April
+* License: Public Domain
+*/
+
+//Define Variables
+make = "Tesla";
+model = "X";
+color = "Black";
+year = 2019;
+
+//calculate
+age = 2017 + 2;
+
+//output
+document.writeln("Make: " make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Age: " + age "years<br>");
