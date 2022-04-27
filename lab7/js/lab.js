9d10df169a790851fa567a5f301f3c2c3235122d
Izzy Lyons <iplyons@ucsc.edu>
2022-04-27 09:41:24 -0700

    u

---
+++ lab7/js/lab.js
@@ -0,0 +1,19 @@
+function sortUserName () {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  // split string to array
+  var nameArraySort = userName.split('');
+  console.log("nameArray =",nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log)"nameArraySort =", nameArraySort);
+  //join array back to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//output
+document.writeln("Oh hey, I have fixed your name:",
+  sortUserName(), "</br>");
+  
