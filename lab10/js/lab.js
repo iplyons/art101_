27db722692ac43fbdd9f6a7b3aebd8683ee779c4
Izzy Lyons <iplyons@ucsc.edu>
2022-05-15 07:53:36 -0700

    u

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -3,21 +3,20 @@
 * Created: May 2
 * License: Public Domain
 */
-// using getElementById() to get the output id from the index file
-let outputE1 = document.getElementById('output');
+//declare variable that holds the button
+var button = document.getElementById("my-button");
 
-// creating a new element assinging it to a newE1 and changing its HTML
-let new1E1 = document.createElement('p');
-new1E1.innerHTML = 'Never gonna give you up!';
+//sort - function that sorts letters of a name
 
-//creating another new element ans assining it to a new2E1 and chaning its HTML
-let new2E1 = document.createElement('p');
-new2E1.innerHTML = 'Never gonna let you down!';
+function sortName(){
+  // take in user input
+  var userName = document.getElementById("user-name").values;
 
-// appending new1E1 and new2E1 to outputE1
-outputE1.appendChild(new1E1);
-outputE1.appendChild(new2E1);
+  var nameSorted = document.createElement('p');
+  nameSorted.innerHTML = userName.split('').sort().join('');
 
-// editing the css using JS
-document.getElementById('challenge').style.color = '#ECEBE4';
-document.getElementById('results').style.color = '#ECEBE4';
+  document.getElementById("output").appendChild(nameSorted);
+
+}
+
+button.addEventListener("click",sortName);
