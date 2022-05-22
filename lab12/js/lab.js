c46996bbb693f85bfa497bec6749554a4ceafc44
Izzy Lyons <iplyons@ucsc.edu>
2022-05-22 16:14:01 -0700

    Update lab.js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -3,44 +3,29 @@
 * Created: May 2
 * License: Public Domain
 */
-$("#challange").append(
-  "<button class = 'blue one'>Blue Button</button>"
-);
-
-$("#probelms").append(
-  "<button class = 'red two'>Red Button</button>"
-);
-
-$("#results").append(
-  "<button class = 'green three'>Green Button</button>"
-);
-
-/* creating click events for buttons that toggle the 'special' class for the section the button is in */
-$("button.blue").click(function(){
-  $("#challange").toggleClass("special");
-  $("#button.two").toggleClass("blue");
-  $("#button.two").toggleClass("red");
-  checkSpecial();
-});
-
-$("button.red").click(function(){
-  $("#probelms").toggleClass("special");
-  $("#button.one").toggleClass("red");
-  $("#button.one").toggleClass("green");
-  checkSpecial();
-});
-
-$("button.green").click(function(){
-  $("#results").toggleClass("special");
-  $("#button.one").toggleClass("green");
-  $("#button.one").toggleClass("blue");
-  checkSpecial();
-});
-
-function checkSpecial(){
-  if ($("#challange").hasClass("special")|| $("#problems").hasClass("special") || $("#results").hasClass("special")){
-    $("#output").removeClass("hidden");
-  } else {
-    $("#output").addClass("hidden");
+// Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+//depending on length mod 4
+function sortingHat(str){
+  len = str.length;
+  mod = len % 4;
+  if(mod == 0){
+    return "Gryffindor"
   }
+  else if (mod == 1){
+    return "Ravenclaw"
+  }
+  else if (mod == 2){
+    return "Slytherin"
 }
+else if (mod == 3){
+  return "Hufflepuff"
+}
+}
+var myButton = document.getElementById("button");
+myButton.addEventListener("click",function(){
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p> The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+  
+})
