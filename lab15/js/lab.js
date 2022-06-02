2c5c57271656a77c0ca2c6dd67dca678e19ee57d
Izzy Lyons <iplyons@ucsc.edu>
2022-06-01 17:06:49 -0700

    Update lab.js

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,3 +3,31 @@
 * Created: May 2
 * License: Public Domain
 */
+function appendText(text){
+  $("#output").html(text);
+}
+
+var apiEndpoint = "https:api.chucknorris.io/jokes/random";
+
+function apiClick(){
+  $.ajax({
+
+    url: apiEndpoint,
+    data: { },
+    type:"GET",
+    dataType : "json",
+    sucess: function( data ){
+      str = ""
+      str += "<p>" + data.value + "</p>";
+
+      appendText(str);
+    }
+  })
+
+  fail(function( xhr, status, errorThrown ){
+    console.log("Failure.");
+    appendText("This sucks.");
+
+  })
+}
+$("activate").click(apiClick);
